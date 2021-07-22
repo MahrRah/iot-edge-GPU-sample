40f31c1b2370ffaabdaa3a1ef304469b70e65f4f
Tamara Rahimi <mahra.rah@gmail.com>
2021-07-22 15:54:16 +0200

    Fix issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     azurerm = {
@@ -18,26 +17,28 @@ resource "random_id" "prefix" {
 
 locals {
   resource_prefix = lower(random_id.prefix.hex)
+  edge_device_name = "${local.resource_prefix}-edge-device"
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${local.resource_prefix}-rg-development"
+  name  = "${local.resource_prefix}-rg-sense-development"
   location = var.resource_group_location
 }
 
 module "iot_hub" {
-  source              = "modules/iot-hub"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  source              = "./modules/iot-hub"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
   resource_prefix     = local.resource_prefix
+  edge_device_name    = local.edge_device_name
 }
 
-module "iot_edge" {
-  source              = "modules/iot-edge"
+module "iot_edge" { 
+  source              = "./modules/iot-edge"
   resource_prefix     = local.resource_prefix
-  iot_hub_name        = module.iot_hub.iot_hub_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
   vm_user_name        = var.edge_vm_user_name
   vm_sku              = var.edge_vm_sku
+  edge_vm_name        = local.edge_device_name
 }
--- terraform/modules/iot-hub/output.tf
+++ terraform/modules/iot-hub/output.tf
@@ -1,20 +1,9 @@
 output "iot_hub_name" {
   value = azurerm_iothub.example.name
 }
-
 output "iot_hub_sku" {
   value = azurerm_iothub.example.sku.0.name
 }
-
-output "iothub_connection_string" {
-  value     = azurerm_iothub_shared_access_policy.example.primary_connection_string
-  sensitive = true
-}
-
-output "iot_hub_resource_id" {
-  value = "subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${azurerm_iothub.example.resource_group_name}/providers/Microsoft.Devices/IotHubs/${azurerm_iothub.sense.name}"
-}
-
 output "iot_hub_id" {
   value = azurerm_iothub.example.id
 }
